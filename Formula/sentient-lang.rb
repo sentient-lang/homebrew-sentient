55313c17c83846ac8265a5b2993434bcb19b7e0f
Paul Mucur <mudge@mudge.name>
2016-11-14 22:40:18 +0000

    Initial formula for install Sentient

---
+++ Formula/sentient-lang.rb
@@ -0,0 +1,22 @@
+require "language/node"
+
+class SentientLang < Formula
+  desc "Sentient Language Compiler and Runtime"
+  homepage "http://sentient-lang.org/"
+  url "https://github.com/sentient-lang/sentient-lang/archive/v0.0.0-alpha.42.tar.gz"
+  version "0.0.0-alpha.42"
+  sha256 "5e4b88a11bffabb07f4c9475c9d4fa013501a5eb04e804cd0be9e42ab24c8931"
+  head "https://github.com/sentient-lang/sentient-lang.git"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    (testpath/"test.snt").write("a = 2 + 2; expose a;")
+    system bin/"sentient", "test.snt"
+  end
+end
---
+++ README.md
@@ -0,0 +1,14 @@
+# Homebrew Sentient
+
+[Homebrew](http://brew.sh/) formulae to install [Sentient](http://sentient-lang.org/).
+
+```console
+$ brew tap sentient-lang/sentient
+$ brew install sentient-lang
+```
+
+## License
+
+Copyright © 2016 Sentient Lang.
+
+This homebrew tap is distributed under the MIT License.
